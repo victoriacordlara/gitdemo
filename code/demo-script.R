3f7b10d0af2a1c4eff78e004473b64f243f72d14
Victoria Cordovez Lara <vicky2K22@berkeley.edu>
2019-10-04 11:08:37 -0700

    R header

---
+++ code/demo-script.R
@@ -0,0 +1,11 @@
+-------------------------------------------------------------------------------
+# Title: Statistical Analysis
+# Description: 
+#     This script computes descriptive and analytical statistics, as well as 
+#     various data visualizations, following the outline for lab 05.
+# Input(s): data from file 'nba2018-players.csv'
+# Output(s): summary data files and graphs
+# Author(s): Victoria Cordovez Lara
+# Date: 10-03-2019
+-------------------------------------------------------------------------------
+  
